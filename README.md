e447da40541d372e0ea5c801fecdccb0632fcb0f
alfredo <alfredo@dashboardhoting.com>
2018-12-12 10:19:13 -0500

    First version of simple chat theme

---
+++ README.md
@@ -0,0 +1,21 @@
+# Simple Chat Theme
+
+It's a simple theme for a application chat, built with html5 css3 and Jquery,where you can switch between different theme
+
+## Example
+
+![alt text](img/screen.PNG "Description goes here")
+
+## Example dark theme
+
+![alt text](img/screen-dark.PNG "Description goes here")
+
+## Installation
+
+Just Donwload it and use
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ js/chat.js
@@ -0,0 +1,74 @@
+
+$(document).ready(function () {
+
+  /*
+  *If localstorage have theme set alredy
+  */
+  if (localStorage.getItem('theme')) {
+    $("head link#theme").attr("href", "css/" + localStorage.getItem('theme'));
+    if (localStorage.getItem('theme') === 'style.css') {
+      $('.badge-ligth').addClass('border-select');
+      // $('#dark').removeClass('border-select');
+    } else {
+      $('.badge-dark').addClass('border-select');
+      // $('#ligth').removeClass('border-select');
+    }
+  } else {
+    $("head link#theme").attr("href", 'css/style.css');
+  }
+
+  // Elements
+
+  var btnHide = $('#hide-contact');
+  var btnShow = $('#show-contact');
+  var listContact = $('.list-contact');
+  var chatDiv = $("#message-container");
+
+  btnShow.on('click', function () {
+
+    listContact.addClass('show');
+
+  });
+
+  btnHide.on('click', function () {
+
+    listContact.removeClass('show');
+  });
+
+  var h = $("#message-container").height();
+
+  // scroll
+  chatDiv.animate({ scrollTop: h + 300 });
+
+
+  //set theme and save in localstorage      
+  $('#dark').on('click', function () {
+    $("head link#theme").attr("href", 'css/dark.css');
+    $('.badge-dark').addClass('border-select');
+    $('.badge-ligth').removeClass('border-select');
+    localStorage.setItem('theme', 'dark.css');
+   
+  });
+
+  $('#ligth').on('click', function () {
+    $("head link#theme").attr("href", 'css/style.css');
+    $('.badge-dark').removeClass('border-select');
+    localStorage.setItem('theme', 'style.css');
+    $('.badge-ligth').addClass('border-select');
+  
+  });
+
+  // if we click outside the dropmenu hide it
+  $(window).click(function () {
+    $('#drop-menu').hide();
+  });
+
+// on click event show dropmenu  
+
+  $('#dropmenu-custom').on('click', function (e) {
+    $('#drop-menu').slideToggle( "fast" );
+    e.stopPropagation();
+  });
+
+});
+
